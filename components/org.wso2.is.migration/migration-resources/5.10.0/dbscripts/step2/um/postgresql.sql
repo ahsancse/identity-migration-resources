218ab676c427a98698a7077d8014d7f93df98408
Apple <ahsan.shamsudeen@gmail.com>
2021-03-17 15:10:09 +0530

    postgresql 9.4 compatibility changes

--- components/org.wso2.is.migration/migration-resources/5.10.0/dbscripts/step2/um/postgresql.sql
+++ components/org.wso2.is.migration/migration-resources/5.10.0/dbscripts/step2/um/postgresql.sql
@@ -1,6 +1,7 @@
 /* User should have the Superuser permission */
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-ALTER TABLE UM_USER
-    ADD COLUMN IF NOT EXISTS UM_USER_ID CHAR(36) DEFAULT uuid_generate_v4(),
-    ADD CONSTRAINT UM_USER_UUID_CONSTRAINT UNIQUE(UM_USER_ID);
+CREATE OR REPLACE FUNCTION alter_um_user_add_user_id() RETURNS void AS $$ BEGIN IF EXISTS (SELECT column_name FROM information_schema.columns WHERE  table_name='um_user' and column_name='um_user_id') THEN RAISE NOTICE 'UM_USER table UM_USER_ID Column already exists'; ELSE ALTER TABLE UM_USER ADD COLUMN  UM_USER_ID CHAR(36) DEFAULT uuid_generate_v4(),ADD CONSTRAINT UM_USER_UUID_CONSTRAINT UNIQUE(UM_USER_ID); END IF;END;$$
+LANGUAGE plpgsql;
+
+select  alter_um_user_add_user_id();
